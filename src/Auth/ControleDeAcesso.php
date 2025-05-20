1ecb8737c2e23ead16f0e0dbd4b2367723773ba6
Vanessa Baldin <vanbaldin08@gmail.com>
2025-05-20 09:34:19 -0300

    adiciona auth

---
+++ src/Auth/ControleDeAcesso.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Microblog\Auth;
+
+//Sobre sessões no PHP sessão (Session) é uma funcionalidade usada para o controle de acesso e outras informações que sejam importantes enquanto o navegador estiver aberto com o site
+
+//Exemplos: Áreas administrativos, painel de controle/dashboard, área do cliente, área do aluno etc.
+
+//Nestas áreas o acesso se dá através de alguma forma de autenticação. Exemplos:
+//login/senha, biometria, facial, 2-fatores etc.
