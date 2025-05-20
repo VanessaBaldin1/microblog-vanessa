ff71ae1c96cd60c6c2ffddd51c881d80d49eddf8
Vanessa Baldin <vanbaldin08@gmail.com>
2025-05-20 11:37:08 -0300

    edita pagina index.php

--- admin/index.php
+++ admin/index.php
@@ -8,7 +8,9 @@ require_once "../includes/cabecalho-admin.php";
 <article class="p-5 my-4 rounded-3 bg-white shadow">
     <div class="container-fluid py-1">        
 
-        <h2 class="display-4">Olá Pessoa Logada!</h2>
+
+         <!-- acessando a sessão e usando os dados do login -->
+        <h2 class="display-4">Olá <?=$_SESSION['nome']?>!</h2>
 
         <?php if( isset($_GET["perfil_atualizado"]) ){ ?>
             <p class="alert alert-primary">Dados atualizados com sucesso!</p>
@@ -16,7 +18,7 @@ require_once "../includes/cabecalho-admin.php";
 
         <p class="fs-5">Você está no <b>painel de controle e administração</b> do
 		site Microblog e seu <b>nível de acesso</b> é 
-        <span class="badge bg-dark"> "admin ou editor" </span>.</p>
+        <span class="badge bg-dark"> <?=$_SESSION['tipo']?> </span>.</p>
         <hr class="my-4">
 
         <div class="d-grid gap-2 d-md-block text-center">
