df89f3b1ac0ef659ac5a49c34773879738db0077
Vanessa Baldin <vanbaldin08@gmail.com>
2025-05-20 09:52:36 -0300

    Adiciona construct Controledeacesso

--- src/Auth/ControleDeAcesso.php
+++ src/Auth/ControleDeAcesso.php
@@ -8,3 +8,32 @@ namespace Microblog\Auth;
 
 //Nestas áreas o acesso se dá através de alguma forma de autenticação. Exemplos:
 //login/senha, biometria, facial, 2-fatores etc.
+
+final class ControleDeAcesso{
+    
+    private function __construct() {}
+
+
+    //Inicia uma sessão caso não tenha nenhuma em andamento
+    private static function iniciarSessao():void
+    {
+         if(!isset($_SESSION)) session_start();
+    }
+    
+
+    //"Bloqueia" páginas admin caso o usuário NÂO ESTEJA logado
+    public function exigirLogin(): void
+
+    {  //Inicia sessão (se necessário)
+       self::iniciarSessao();
+
+        //Se NÃO EXISTIR uma variavel de sessão chamada ID
+       if(!isset($_SESSION['id'])){
+          session_destroy();
+          header("location:../login.php?acesso_proibido");
+          exit;
+
+       }
+    }
+
+}
